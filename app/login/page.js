3581e24b5aee90da787b072e05310989867149c6
AppCreatorLG <herclosetimage@gmail.com>
2026-04-06 00:14:15 -0400

    Fix login page syntax

--- app/login/page.js
+++ app/login/page.js
@@ -9,17 +9,6 @@ export default function LoginPage() {
         ClosetLogic
       </h1>
 
-      "use client";
-
-import { signIn } from "next-auth/react";
-
-export default function LoginPage() {
-  return (
-    <div className="min-h-screen flex flex-col justify-center px-6">
-      <h1 className="text-3xl font-bold text-center mb-6">
-        ClosetLogic
-      </h1>
-
       <button
         onClick={() =>
           signIn("google", {
